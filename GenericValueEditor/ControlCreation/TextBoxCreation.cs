12f15f5b41f98f7d215a1dea478721454e0751af
SMG <scandanavianmountaingoat@gmail.com>
2018-11-06 17:44:39 -0600

    simplify textbox parsing

--- GenericValueEditor/ControlCreation/TextBoxCreation.cs
+++ GenericValueEditor/ControlCreation/TextBoxCreation.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Forms;
 using GenericValueEditor.Utils;
+using System;
 
 namespace GenericValueEditor.ControlCreation
 {
     internal static class TextBoxCreation
     {
+        // Use a dictionary to avoid a large switch statement and simplify arguments.
+        private static readonly Dictionary<ValueEnums.ValueType, Func<TextBox, object>> parseFunctionByType = new Dictionary<ValueEnums.ValueType, Func<TextBox, object>>()
+        {
+            { ValueEnums.ValueType.Float, textBox => TextBoxUtils.TryParseFloat(textBox) },
+            { ValueEnums.ValueType.Double, textBox => TextBoxUtils.TryParseDouble(textBox) },
+            { ValueEnums.ValueType.Int, textBox => TextBoxUtils.TryParseInt(textBox) },
+            { ValueEnums.ValueType.UintFlag, textBox => TextBoxUtils.TryParseUint(textBox, true) },
+            { ValueEnums.ValueType.String, textBox => textBox.Text }
+        };
+
         public static TextBox AddTextBox(TableLayoutPanel tableLayout, ValueEnums.ValueType type, int row, int col,
             string name, Dictionary<string, EditorValue> valueByName)
         {
@@ -26,42 +37,11 @@ namespace GenericValueEditor.ControlCreation
         private static void CreateTextChangedEvent(ValueEnums.ValueType type, string name, TextBox control,
             Dictionary<string, EditorValue> valueByName)
         {
-            // TODO: This could be simplified using Dictionary<Type, Func>.
-            switch (type)
+            control.TextChanged += (sender, args) =>
             {
-                case ValueEnums.ValueType.UintFlag:
-                    control.TextChanged += (sender, args) =>
-                    {
-                        valueByName[name].Value = TextBoxUtils.TryParseUint((TextBox)sender, true);
-                    };
-                    break;
-                case ValueEnums.ValueType.Float:
-                    control.TextChanged += (sender, args) =>
-                    {
-                        valueByName[name].Value = TextBoxUtils.TryParseFloat((TextBox)sender);
-                    };
-                    break;
-                case ValueEnums.ValueType.Double:
-                    control.TextChanged += (sender, args) =>
-                    {
-                        valueByName[name].Value = TextBoxUtils.TryParseDouble((TextBox)sender);
-                    };
-                    break;
-                case ValueEnums.ValueType.Int:
-                    control.TextChanged += (sender, args) =>
-                    {
-                        valueByName[name].Value = TextBoxUtils.TryParseInt((TextBox)sender);
-                    };
-                    break;
-                case ValueEnums.ValueType.String:
-                    control.TextChanged += (sender, args) =>
-                    {
-                        valueByName[name].Value = ((TextBox)sender).Text;
-                    };
-                    break;
-                default:
-                    break;
-            }
+                // TODO: What happens when the type isn't in the dictionary?
+                valueByName[name].Value = parseFunctionByType[type](control);
+            };
         }
     }
 }
