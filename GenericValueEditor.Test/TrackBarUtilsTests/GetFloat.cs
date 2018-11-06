725915b8bb844b21cc4da0c154696a808088c01f
SMG <scandanavianmountaingoat@gmail.com>
2018-11-06 08:58:28 -0600

    test get/set trackbar floats

---
+++ GenericValueEditor.Test/TrackBarUtilsTests/GetFloat.cs
@@ -0,0 +1,57 @@
+﻿using GenericValueEditor.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericValueEditor.Test.TrackBarUtilsTest
+{
+    [TestClass]
+    public class GetFloat
+    {
+        [TestMethod]
+        public void MinValue0To1()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            trackBar.Value = trackBar.Minimum;
+            Assert.AreEqual(0f, TrackBarUtils.GetFloat(trackBar, 0, 1));
+        }
+
+        [TestMethod]
+        public void MidValue0To1()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            trackBar.Value = trackBar.Maximum / 2;
+            Assert.AreEqual(0.5f, TrackBarUtils.GetFloat(trackBar, 0, 1));
+        }
+
+        [TestMethod]
+        public void MaxValue0To1()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            trackBar.Value = trackBar.Maximum;
+            Assert.AreEqual(1f, TrackBarUtils.GetFloat(trackBar, 0, 1));
+        }
+
+        [TestMethod]
+        public void MinValueNeg5To5()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            trackBar.Value = trackBar.Minimum;
+            Assert.AreEqual(-5f, TrackBarUtils.GetFloat(trackBar, -5, 5));
+        }
+
+        [TestMethod]
+        public void MidValueNeg5To5()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            trackBar.Value = trackBar.Maximum / 2;
+            Assert.AreEqual(0f, TrackBarUtils.GetFloat(trackBar, -5, 5));
+        }
+
+        [TestMethod]
+        public void MaxValueNeg5To5()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            trackBar.Value = trackBar.Maximum;
+            Assert.AreEqual(5f, TrackBarUtils.GetFloat(trackBar, -5, 5));
+        }
+    }
+}
---
+++ GenericValueEditor.Test/TrackBarUtilsTests/SetFloat.cs
@@ -0,0 +1,57 @@
+﻿using GenericValueEditor.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericValueEditor.Test.TrackBarUtilsTest
+{
+    [TestClass]
+    public class SetFloat
+    {
+        [TestMethod]
+        public void MinValue0To1()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            TrackBarUtils.SetFloat(0, trackBar, 0, 1);
+            Assert.AreEqual(trackBar.Minimum, trackBar.Value);
+        }
+
+        [TestMethod]
+        public void MidValue0To1()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            TrackBarUtils.SetFloat(0.5f, trackBar, 0, 1);
+            Assert.AreEqual(trackBar.Maximum / 2, trackBar.Value);
+        }
+
+        [TestMethod]
+        public void MaxValue0To1()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            TrackBarUtils.SetFloat(1, trackBar, 0, 1);
+            Assert.AreEqual(trackBar.Maximum, trackBar.Value);
+        }
+
+        [TestMethod]
+        public void MinValueNeg5To5()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            TrackBarUtils.SetFloat(-5, trackBar, -5, 5);
+            Assert.AreEqual(trackBar.Minimum, trackBar.Value);
+        }
+
+        [TestMethod]
+        public void MidValueNeg5To5()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            TrackBarUtils.SetFloat(0, trackBar, -5, 5);
+            Assert.AreEqual(trackBar.Maximum / 2, trackBar.Value);
+        }
+
+        [TestMethod]
+        public void MaxValueNeg5To5()
+        {
+            var trackBar = new System.Windows.Forms.TrackBar();
+            TrackBarUtils.SetFloat(5, trackBar, -5, 5);
+            Assert.AreEqual(trackBar.Maximum, trackBar.Value);
+        }
+    }
+}
