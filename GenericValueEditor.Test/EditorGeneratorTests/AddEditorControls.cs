d8508dc1b89cc7b44c9d54f4d43a83a615759194
SMG <scandanavianmountaingoat@gmail.com>
2018-12-04 21:29:08 -0600

    add basic tests for editor generator

---
+++ GenericValueEditor.Test/EditorGeneratorTests/AddEditorControls.cs
@@ -0,0 +1,50 @@
+﻿using GenericValueEditor;
+using GenericValueEditor.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericValueEditor.Test.EditorGeneratorTests
+{
+    [TestClass]
+    public class AddEditorControls
+    {
+        private class NoMembers
+        {
+        }
+
+        private class ValidClass
+        {
+            [EditInfo("edit me", ValueEnums.ValueType.Bool)]
+            public bool ReadonlyFlag { get; set; }
+        }
+
+        [TestMethod]
+        public void NoValidAttributes()
+        {
+            var generator = new EditorGenerator<object>(new object());
+
+            var parent = new System.Windows.Forms.Control();
+            generator.AddEditorControls(parent);
+            Assert.AreEqual(0, parent.Controls.Count);
+        }
+
+        [TestMethod]
+        public void NoMembersInClass()
+        {
+            var generator = new EditorGenerator<NoMembers>(new NoMembers());
+
+            var parent = new System.Windows.Forms.Control();
+            generator.AddEditorControls(parent);
+            Assert.AreEqual(0, parent.Controls.Count);
+        }
+
+        [TestMethod]
+        public void ValidMembersInClass()
+        {
+            var generator = new EditorGenerator<ValidClass>(new ValidClass());
+
+            var parent = new System.Windows.Forms.Control();
+            generator.AddEditorControls(parent);
+            Assert.AreNotEqual(0, parent.Controls.Count);
+        }
+    }
+}
